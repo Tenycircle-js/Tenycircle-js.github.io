c7ce7a27df1c7dcb7eb6d84d00480f792ee2684e
Akawatav <127651665+Akawatav@users.noreply.github.com>
2023-05-13 22:59:39 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded",_=>{
+  document.querySelectorAll("include").forEach(element=>{
+    switch(element.getAttribute("type")) {
+      case "html": {
+        fetch(element.getAttribute("href"))
+        .then(response=>response.text())
+        .then(data=>element.innerHTML=data)
+        .catch(console.error)
+        break
+      }
+      case "js":
+      case "javascript": {
+        let script = document.createElement("script")
+        script.setAttribute("src",element.getAttribute("href"))
+        document.head.appendChild(script)
+        break
+      }
+      case "css":{ 
+        let link = document.createElement("link")
+        link.setAttribute("rel","stylesheet")
+        link.setAttribute("href",element.getAttribute("href"))
+        document.head.appendChild(link)
+        break
+      }
+    }
+  })
+})
