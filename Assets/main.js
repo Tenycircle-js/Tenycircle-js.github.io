ed99557f1d0012a60eaa56ab85da1d271fc5b6ef
Akawatav <127651665+Akawatav@users.noreply.github.com>
2023-05-14 11:53:01 +0900

    Update main.js

--- Assets/main.js
+++ Assets/main.js
@@ -1,6 +1,27 @@
-function include(htmlname, tagname) {
-  fetch(htmlname)
-  .then(response=>response.text())
-  .then(data=>document.querySelector(`include#${tagname}`).innerHTML=data)
-  .catch(console.error);
-}
+window.addEventListener("DOMContentLoaded",_=>{
+  document.querySelectorAll("include").forEach(element=>{
+    switch(element.getAttribute("type")) {
+      case "html": {
+        fetch(element.getAttribute("href"))
+        .then(response=>response.text())
+        .then(data=>element.innerHTML=data)
+        .catch(console.error)
+        break
+      }
+      case "js":
+      case "javascript": {
+        let script = document.createElement("script")
+        script.setAttribute("src",element.getAttribute("href"))
+        document.head.appendChild(script)
+        break
+      }
+      case "css":{ 
+        let link = document.createElement("link")
+        link.setAttribute("rel","stylesheet")
+        link.setAttribute("href",element.getAttribute("href"))
+        document.head.appendChild(link)
+        break
+      }
+    }
+  })
+})
