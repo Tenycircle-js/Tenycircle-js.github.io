dfc685fb4ded173d36e4effe6c012f9ccf0ce22b
Akawatav <127651665+Akawatav@users.noreply.github.com>
2023-06-16 22:21:06 +0900

    Update main.js

--- Assets/main.js
+++ Assets/main.js
@@ -1,8 +1,8 @@
 window.addEventListener("load",_=>{
-  document.querySelectorAll(":is(include,.include)[type]").forEach(element=>{
-    switch(element.getAttribute("type")) {
+  document.querySelectorAll(":is(include,.include)[data-type]").forEach(element=>{
+    switch(element.dataset.type")) {
       case "html": {
-        fetch(element.getAttribute("href"))
+        fetch(element.dataset.src))
         .then(response=>response.text())
         .then(data=>element.innerHTML=data)
         .catch(console.error)
@@ -24,8 +24,8 @@ window.addEventListener("load",_=>{
       }
     }
   })
-  document.querySelectorAll`br[row]`.forEach(element=>{
-    for(let i = ((+(element.getAttribute`row`))-1);i--;) {
+  document.querySelectorAll("br[data-row]"!.forEach(element=>{
+    for(let i = ((+element.dataset.row)-1);i--;) {
       let br = document.createElement`br`
       element.parentNode.insertBefore(br,element);
     }
