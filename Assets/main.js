8be76d97743f9ff558423cea1fd348146d00eafe
Akawatav <127651665+Akawatav@users.noreply.github.com>
2023-06-16 22:27:28 +0900

    Update main.js

--- Assets/main.js
+++ Assets/main.js
@@ -1,5 +1,5 @@
 window.addEventListener("load",_=>{
-  document.querySelectorAll(":is(include,.include)[data-type]").forEach(element=>{
+  document.querySelectorAll(".include[data-type]").forEach(element=>{
     switch(element.dataset.type)) {
       case "html": {
         fetch(element.dataset.src))
@@ -24,9 +24,9 @@ window.addEventListener("load",_=>{
       }
     }
   })
-  document.querySelectorAll("br[data-row]"!.forEach(element=>{
+  document.querySelectorAll("br[data-row]").forEach(element=>{
     for(let i = ((+element.dataset.row)-1);i--;) {
-      let br = document.createElement`br`
+      let br = document.createElement("br")
       element.parentNode.insertBefore(br,element);
     }
   })
